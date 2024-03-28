6b5f90bc508785873a07557d7752092c20f893db
HTTKIM <151608618+HTTKIM@users.noreply.github.com>
2024-03-28 16:12:01 +0900

    Add files via upload

---
+++ law_1.py
@@ -0,0 +1,85 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from openai import OpenAI
+import time
+from typing_extensions import override
+from openai import AssistantEventHandler
+
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+ 
+def app():
+    load_dotenv()
+    API_KEY = os.environ['OPENAI_API_KEY']
+
+    client = OpenAI(api_key=API_KEY)
+
+    # thread id 생성하여 하나로 관리
+    if 'thread_id' not in st.session_state:
+        thread = client.beta.threads.create()
+        st.session_state.thread_id = thread.id  
+        
+    # thread_id, assistant_id 설정
+    thread_id = st.session_state.thread_id
+    assistant_id = "asst_L0rUykoOT76rKCJ3UKhOiVSK"
+
+    # 메세지 불러오기
+    thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
+
+    # 페이지 제목
+    st.subheader(":blue[법령(건축 및 가스)] :gray[검색 Chatbot]")
+
+    # 메세지 역순으로 가져와 UI 에 입력
+    for msg in thread_messages.data:
+        with st.chat_message(msg.role):
+            st.write(msg.content[0].text.value)
+
+    # 입력창에 입력을 받아 입력된 내용으로 메세지 생성        
+    prompt = st.chat_input("[법령(건축 및 가스)] 질문을 입력하세요!")
+    if prompt:
+        message = client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=prompt
+        )
+        
+        # 입력한 메세지 UI에 표시
+        with st.chat_message(message.role):
+            st.write("[법령(건축 및 가스)] " + message.content[0].text.value)
+        
+
+        with client.beta.threads.runs.create_and_stream(
+            thread_id=thread_id,
+            assistant_id=assistant_id,
+            event_handler=EventHandler(),
+            ) as stream:
+            st.write(stream.until_done())
+        
+        # # While 문 완료 후 메세지 불러오기
+        # messages = client.beta.threads.messages.list(
+        #     thread_id=thread_id   
+        # )
+        
+        # # 마지막 메세지 UI에 추가하기
+        # with st.chat_message(messages.data[0].role):
+        #     st.write(messages.data[0].content[0].text.value)
