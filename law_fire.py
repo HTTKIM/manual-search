976b0cf90e7fd85322becf821d944e7487e2aec2
HTTKIM <151608618+HTTKIM@users.noreply.github.com>
2024-04-04 09:06:23 +0900

    Update law_fire.py

--- law_fire.py
+++ law_fire.py
@@ -28,50 +28,50 @@ class EventHandler(AssistantEventHandler):
           if output.type == "logs":
             print(f"\n{output.logs}", flush=True)
  
-
-load_dotenv()
-API_KEY = os.environ['OPENAI_API_KEY']
-
-client = OpenAI(api_key=API_KEY)
-
-# thread id 생성하여 하나로 관리
-if 'thread_id' not in st.session_state:
-    thread = client.beta.threads.create()
-    st.session_state.thread_id = thread.id  
-    
-# thread_id, assistant_id 설정
-thread_id = st.session_state.thread_id
-assistant_id = "asst_CLFwAoRwRdWTprQd53D5epLr"
-
-# 메세지 불러오기
-thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
-
-# 페이지 제목
-st.subheader(":blue[화보법] :gray[검색 Chatbot]")
-
-# 메세지 역순으로 가져와 UI 에 입력
-for msg in thread_messages.data:
-    with st.chat_message(msg.role):
-        st.write(msg.content[0].text.value)
-
-# 입력창에 입력을 받아 입력된 내용으로 메세지 생성        
-prompt = st.chat_input("[화보법] 질문을 입력하세요!")
-
-if prompt:
-    message = client.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content=prompt
-    )
-    
-    # 입력한 메세지 UI에 표시
-    with st.chat_message(message.role):
-        st.write("[화보법] " + message.content[0].text.value)
-    
-    with client.beta.threads.runs.stream(
-        thread_id=thread_id,
-        assistant_id=assistant_id,
-        event_handler=EventHandler(),
-        ) as stream:
-        st.write_stream(stream.text_deltas)
-        stream.until_done()
+def app():
+  load_dotenv()
+  API_KEY = os.environ['OPENAI_API_KEY']
+  
+  client = OpenAI(api_key=API_KEY)
+  
+  # thread id 생성하여 하나로 관리
+  if 'thread_id' not in st.session_state:
+      thread = client.beta.threads.create()
+      st.session_state.thread_id = thread.id  
+      
+  # thread_id, assistant_id 설정
+  thread_id = st.session_state.thread_id
+  assistant_id = "asst_CLFwAoRwRdWTprQd53D5epLr"
+  
+  # 메세지 불러오기
+  thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
+  
+  # 페이지 제목
+  st.subheader(":blue[화보법] :gray[검색 Chatbot]")
+  
+  # 메세지 역순으로 가져와 UI 에 입력
+  for msg in thread_messages.data:
+      with st.chat_message(msg.role):
+          st.write(msg.content[0].text.value)
+  
+  # 입력창에 입력을 받아 입력된 내용으로 메세지 생성        
+  prompt = st.chat_input("[화보법] 질문을 입력하세요!")
+  
+  if prompt:
+      message = client.beta.threads.messages.create(
+          thread_id=thread_id,
+          role="user",
+          content=prompt
+      )
+      
+      # 입력한 메세지 UI에 표시
+      with st.chat_message(message.role):
+          st.write("[화보법] " + message.content[0].text.value)
+      
+      with client.beta.threads.runs.stream(
+          thread_id=thread_id,
+          assistant_id=assistant_id,
+          event_handler=EventHandler(),
+          ) as stream:
+          st.write_stream(stream.text_deltas)
+          stream.until_done()
